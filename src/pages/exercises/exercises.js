d19789320c9dc88039ed8a9deebe3d8f8743fdf3
Dave Heseltine <daveomcdave@gmail.com>
2023-03-29 19:24:23 +0100

    Added a loading indicator.

--- src/pages/exercises/exercises.js
+++ src/pages/exercises/exercises.js
@@ -2,14 +2,15 @@
 import React, { useEffect, useState } from "react";
 import API from "../../utils/API.js";
 import Masonry from "react-masonry-css";
-import "./exercises.css";
+import GridLoader from "react-spinners/GridLoader";
 
-import { Container } from 'react-bootstrap';
-import Row from '../../components/Row';
+// Require:
+const noResultsImg = require('../../assets/attackofsomeclones.jpg')
+
 
 function Exercises() {
     // API loading indicator:
-    // const [apiLoading, setApiLoading] = useState(false);
+    const [apiLoading, setApiLoading] = useState(false);
 
     // API parameters:
     const [apiParams, setApiParams] = useState({
@@ -19,11 +20,12 @@ function Exercises() {
     });
 
     // Masonry content (of API results):
+    const [noResults, setNoResults] = useState(false);
     const [masonryContent, setMasonryContent] = useState([]);
 
     // Radio:
     const optionsType = ["cardio", "olympic_weightlifting", "plyometrics", "powerlifting", "strength", "stretching", "strongman"];
-    const optionsMuscle = ["abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "lower_back", "middle_back", "neck", "quadriceps", "traps", "triceps"];
+    const optionsMuscle = ["abdominals", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "lower_back", "middle_back", "neck", "quadriceps", "traps", "triceps"];
     const optionsDifficulty = ["beginner", "intermediate", "expert"];
 
     // Masonry formatting:
@@ -31,6 +33,14 @@ function Exercises() {
         default: 3,
         1200: 2,
         992: 1
+    }
+
+    // Function to update API parameters to be called:
+    const handleChange = (name, value) => {
+        setApiParams({
+            ...apiParams,
+            [name]: value
+        });    
     }
 
     const radioGenerate = (array, param) => {
@@ -54,51 +64,54 @@ function Exercises() {
             );
 
         return (
-            <div class="pt-2">
-                <div class="px-3 py-2" style={{backgroundColor: "#f2f2f2", border: "solid 3px lightgrey", borderRadius: "20px"}}>
+            <div className="pt-2">
+                <div className="px-3 py-2" style={{backgroundColor: "#f2f2f2", border: "solid 3px lightgrey", borderRadius: "20px"}}>
                     {radioGenerated}
                 </div>
             </div>
         )
     }
 
-
-    // Function to update API parameters to be called:
-    const handleChange = (name, value) => {
-        setApiParams({
-            ...apiParams,
-            [name]: value
-        });    
-    }
-
     // Initialisation:
     // Function to run on Start and "onChange":
     useEffect(() => {
-        // setApiLoading(true);
+        setMasonryContent([]);
+        setNoResults([]);
+        setApiLoading(() => true);
         let resData = [];
         API(apiParams, 0)
             .then (res => {
+                setApiLoading(() => false);
                 resData.push(...res.data);
                 resData = resData.sort((a, b) => a.name.localeCompare(b.name));
-                setMasonryContent(resData.map(
-                    function(exercise, index) {
-                        return (
-                            <div key={index} class="p-3">
-                                <h1 class="col-12 text-center pb-3">{exercise.name}</h1>
-                                <div class="d-flex flex-wrap text-center pb-3">
-                                    <p class="col-6"><strong>Type:</strong><br/>{exercise.type}</p>
-                                    <p class="col-6"><strong>Muscle:</strong><br/>{exercise.muscle}</p>
-                                    <p class="col-6"><strong>Equipment:</strong><br/>{exercise.equipment}</p>
-                                    <p class="col-6"><strong>Difficulty:</strong><br/>{exercise.difficulty}</p>                                
-                                </div>
+                console.log(resData.length);
+                if (resData.length === 0) {
+                    setNoResults(
+                        <div className="col-12 d-flex flex-wrap justify-content-center">
+                            <h3 className="col-10 text-center py-5">I hate to say it, but it looks like the exercise you're searching for doesn't exist.</h3>                            
+                            <img className="col-10" src={noResultsImg} alt="Hello there!" style={{borderRadius: "30px"}}/>
+                        </div>                        
+                    );
+                } else {
 
-                                <p  class="col-12"><strong>Instructions:</strong><br/>{exercise.instructions}</p>
-                            </div>
-                        )
-                    }
-                ))
+                    setMasonryContent(resData.map(
+                        function(exercise, index) {
+                            return (
+                                <div key={index} className="p-3">
+                                    <h1 className="col-12 text-center pb-3">{exercise.name}</h1>
+                                    <div className="d-flex flex-wrap text-center pb-3">
+                                        <p className="col-6"><strong>Type:</strong><br/>{exercise.type}</p>
+                                        <p className="col-6"><strong>Muscle:</strong><br/>{exercise.muscle}</p>
+                                        <p className="col-6"><strong>Equipment:</strong><br/>{exercise.equipment}</p>
+                                        <p className="col-6"><strong>Difficulty:</strong><br/>{exercise.difficulty}</p>                                
+                                    </div>
+                                    <p className="col-12"><strong>Instructions:</strong><br/>{exercise.instructions}</p>
+                                </div>
+                            )
+                        }
+                    ))
+                }
             })
-        // setApiLoading(false);
     }, [apiParams])
 
     const radioType = radioGenerate(optionsType, "type");
@@ -106,34 +119,24 @@ function Exercises() {
     const radioDifficulty = radioGenerate(optionsDifficulty, "difficulty");
 
     return (
-        <div>
-            <div>
-            <Container id='formbackground'  style={{fontFamily: 'Georgia'}}>
-                <Row>
-                <div>
-                    <h2>Exercises</h2>
-                    <p>Find an inspiration for your workout</p>
-                    <p></p>
-                </div>
-                </Row>
-            </Container>
-          </div>  
-
-        <div class="d-flex flex-wrap" style={{backgroundColor: "#aec4e8"}}>
+        <div className="d-flex flex-wrap">
             {/* Radios: */}
-            <div class="col-lg-3 col-md-4 col-12 px-3" style={{positon: "fixed !important", height: "100vh"}}>
+            <div className="col-lg-3 col-md-4 col-12 px-3">
                 {radioType}
                 {radioMuscle}
                 {radioDifficulty}
             </div>
 
             {/* Masonry: */}
-            <div class="col-lg-9 col-md-8 col-12 px-2 py-2">
+            <div className="col-lg-9 col-md-8 col-12 px-2 py-2">
+                <div className="d-flex justify-content-center">
+                    <GridLoader className="m-5" color={"#3E83C1"} loading={apiLoading} size={100} />
+                    {noResults}
+                </div>        
                 <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
                     {masonryContent}
                 </Masonry>
             </div>
-        </div>
         </div>
     )
 }
